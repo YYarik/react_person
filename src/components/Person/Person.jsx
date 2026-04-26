207583c0d3574ed9d248798c4ae70d627af4b44f
Yaroslav <gaubetsa@gmail.com>
2026-04-26 22:11:23 +0300

    fixed2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable prettier/prettier */
-export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
+export const Person = (
+  { person: { name, age, sex, isMarried, partnerName }},
+) => {
+  const isMarriedStatus = isMarried;
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
-      {isMarried === true ? (
+      {isMarriedStatus ? (
         <p className="Person__partner">
-          {partnerName} is my
-          {sex === 'm'
+          {partnerName} is my {sex === 'm'
             ? 'wife'
             : 'husband'
           }
